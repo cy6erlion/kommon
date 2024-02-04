390fd480d720f73f29a71460afafa2f55bfb26b9
Jackson G. Kaindume <cy6erlion@protonmail.com>
2024-02-04 19:17:22 +0200

    [ffi]: add function to convert a *const c_char to a Rust String

--- src/ffi/mod.rs
+++ src/ffi/mod.rs
@@ -1,4 +1,16 @@
 //! kommon tools for working with FFI
+use std::ffi::CStr;
+use std::ffi::c_char;
 
 pub use result::{FFIJSON, FFIResult};
 mod result;
+
+/// Convert a *const c_char to a rust String
+pub unsafe fn as_rust_str(c: *const c_char) -> String {
+    let cs = unsafe {
+	assert!(!c.is_null());
+	CStr::from_ptr(c)
+    };
+    let s = cs.to_str().unwrap();
+    s.to_string()
+}
