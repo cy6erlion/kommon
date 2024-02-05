cc9f4fd1b0245ee1f9fb84a27830daf2aed59156
Jackson G. Kaindume <cy6erlion@protonmail.com>
2024-02-05 19:25:49 +0200

    [ffi]: Simplify FFIResult
    Change it from an enum to a struct.

--- src/ffi/result.rs
+++ src/ffi/result.rs
@@ -1,40 +1,32 @@
 //! Json serialized Rust result intented to be send via FFI
 
 use serde::Serialize;
+use std::error::Error;
 use std::ffi::c_char;
 use std::ffi::CString;
-use std::error::Error;
 
 /// Json string to be send via FFI
 pub type FFIJSON = *mut c_char;
 
 /// A result send accross FFI
-pub enum FFIResult<T: Serialize, E: Error>{
-    /// Successfull result with a JSON payload
-    Json(T),
-    /// Successfull result with no payload
-    Okay,
-    /// Error result with the error included
-    Error(E)
-}
+pub struct FFIResult;
 
-impl <T,E> FFIResult<T, E>
-where
-    T: Serialize,
-    E: Error{
-    /// Export result as a FFI compatible JSON string
-    pub fn export(&self) -> FFIJSON{
-	match self{
-	    Self::Json(j) => {
-		CString::new(serde_json::to_string(&j).unwrap())
-		    .unwrap()
-		    .into_raw()
-	    },
-	    Self::Okay => CString::new("{\"ffiOk\":true}").unwrap().into_raw(),
-	    Self::Error(err) => {
-		let err_msg = format!("{{\"ffierror\": {err}}}");
-		CString::new(err_msg).unwrap().into_raw()
-	    }
-	}
+impl FFIResult {
+    /// Export JSON via FFI
+    pub fn json<T: Serialize>(j: T) -> FFIJSON {
+        CString::new(serde_json::to_string(&j).unwrap())
+            .unwrap()
+            .into_raw()
+    }
+
+    /// An okay result
+    pub fn okay() -> FFIJSON {
+        CString::new("{\"ffiOk\":true}").unwrap().into_raw()
+    }
+
+    /// Export error as a FFI compatible JSON string
+    pub fn error<E: Error>(err: E) -> FFIJSON {
+        let err_msg = format!("{{\"ffierror\": {err}}}");
+        CString::new(err_msg).unwrap().into_raw()
     }
 }
