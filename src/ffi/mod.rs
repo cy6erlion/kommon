4dbdd3949911e4d6e4a9cf6a157420d4394e1cae
Jackson G. Kaindume <cy6erlion@protonmail.com>
2024-04-24 03:19:51 +0200

    update packages and formating

--- src/ffi/mod.rs
+++ src/ffi/mod.rs
@@ -1,15 +1,15 @@
 //! kommon tools for working with FFI
-use std::ffi::CStr;
 use std::ffi::c_char;
+use std::ffi::CStr;
 
-pub use result::{FFIJSON, FFIResult};
+pub use result::{FFIResult, FFIJSON};
 mod result;
 
 /// Convert a *const c_char to a rust String
 pub unsafe fn as_rust_str(c: *const c_char) -> String {
     let cs = unsafe {
-	assert!(!c.is_null());
-	CStr::from_ptr(c)
+        assert!(!c.is_null());
+        CStr::from_ptr(c)
     };
     let s = cs.to_str().unwrap();
     s.to_string()
